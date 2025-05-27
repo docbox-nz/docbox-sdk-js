b53ad77e907dd5e6fb555733d7c984598d18e9c8
Jacobtread <jacobtread@gmail.com>
2025-05-28 00:03:22 +1200

    feat: add item union type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,7 @@
+import { DocFile } from './file';
+import { DocFolder } from './folder';
+import { DocLink } from './link';
+
 export * from './box';
 export * from './file';
 export * from './folder';
@@ -5,3 +9,14 @@ export * from './link';
 export * from './search';
 export * from './shared';
 export * from './user';
+
+export enum DocboxItemType {
+  File = 'File',
+  Folder = 'Folder',
+  Link = 'Link',
+}
+
+export type DocboxItem =
+  | ({ type: DocboxItemType.File } & DocFile)
+  | ({ type: DocboxItemType.Folder } & DocFolder)
+  | ({ type: DocboxItemType.Link } & DocLink);
